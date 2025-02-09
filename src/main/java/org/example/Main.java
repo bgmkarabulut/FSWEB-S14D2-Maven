4e82b545e05682795843bf4104b011cf303ea963
example <email@example.com>
2025-02-09 14:55:25 +0300

    pushed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp=new Lamp(LampType.NORMAL,true,15);
+        System.out.println(lamp.getGlobRating());
+        System.out.println(lamp.getStyle());
+        System.out.println(lamp.isBattery());
+        lamp.turnOn();
+
+        Bedroom bedroom =new Bedroom("Bella",new Wall("kuzey"),new Wall("güney"),new Wall("doğu"),new Wall("batı"),
+                new Ceiling(12, PaintColor.RED),new Bed("King",3,180,2,1),
+                new Lamp(LampType.NEON,true,14),new Wardrobe(5,6,8.5),new Carpet(100,90,PaintColor.RED));
+        bedroom.getBed();
+        bedroom.getCarpet();
+        bedroom.getCeiling();
+        bedroom.getLamp();
+        bedroom.getName();
+        bedroom.getWall1();
+        bedroom.getWall2();
+        bedroom.getWall3();
+        bedroom.getWall4();
+        bedroom.getWardrobe();
     }
 }
